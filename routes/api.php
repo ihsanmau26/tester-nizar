a30626f121c94c09f6dbc67529f1db3f7d549cb3
unknown <ikhsanmaulana000002@gmail.com>
2026-03-20 12:52:45 +0700

    fixing bugs

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [App\Http\Controllers\Api\AuthController::class, 'me']);
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+
     Route::apiResource('patients', App\Http\Controllers\Api\PatientController::class);
     Route::apiResource('health-records', App\Http\Controllers\Api\HealthRecordController::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-redis', function () {
+    Redis::set('nama', 'Budi');
+    return Redis::get('nama');
+});
